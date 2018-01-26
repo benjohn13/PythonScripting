32406eca8e1e0d2e930c5969ff2f633afbb33c1c
Ben <25944944+benjohn13@users.noreply.github.com>
2018-01-26 14:44:35 -0600

    Create variable_script.py

---
+++ variables/variable_script.py
@@ -0,0 +1,10 @@
+# In Linux, use vim to create a new file called some_script.py 
+
+#!/usr/bin/env python
+first_name = "Ben"
+hobby = "Linux"
+code = "Python"
+fav_topic = "security"
+
+print("My name is %s.\nI like to code in %s while sudying %s and %s!"\
+ % (first_name, code, hobby, fav_topic))
